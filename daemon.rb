aa127c8c05be281460b4671569cf15324722d186
David Kontyko <33853225+dkontyko@users.noreply.github.com>
2022-05-10 22:58:05 -0400

    fixing Rubocop warnings

--- daemon.rb
+++ daemon.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 require 'rubygems'
 require 'daemons'
 
 pwd = Dir.pwd
-Daemons.run_proc('pdf-filler', {:dir_mode => :normal, :dir => "/opt/pids/sinatra"}) do
+Daemons.run_proc('pdf-filler', { dir_mode: :normal, dir: '/opt/pids/sinatra' }) do
   Dir.chdir(pwd)
-  exec "ruby app.rb"
+  exec 'ruby app.rb'
 end
